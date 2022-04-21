7f03b5956419889465b8552a69170c9c98fba62d
Denis <bardenbmw@gmail.com>
2022-04-21 10:40:01 +0400

    Исправление конечной версии 1.0

--- ConsoleApplication3/SPO_LR_5.cpp
+++ ConsoleApplication3/SPO_LR_5.cpp
@@ -6,6 +6,8 @@ void OneExemple();
 
 void TwoExemple();
 
+void Print(bool mem);
+
 MEMORY_BASIC_INFORMATION mbi;
 DWORD pb;
 int count;
@@ -26,21 +28,7 @@ void OneExemple() {
 	count = 0;
 	memory = 0;
 	pb = 0;
-	printf_s("%8s %20s \t%16s\n", "Адрес", "Размер региона", "Атрибут защиты");
-	while (VirtualQuery(LPCVOID(pb), &mbi, sizeof(MEMORY_BASIC_INFORMATION)) == sizeof(mbi))
-	{
-		if (mbi.State == MEM_FREE)
-		{
-			if (pb > 0 && pb < pow(1024, 3))
-				printf_s(" %08x %16d %16d\n", mbi.BaseAddress, mbi.RegionSize, mbi.State);
-
-			count++;
-			memory += mbi.RegionSize;
-		}
-		pb = pb + mbi.RegionSize;
-	}
-	printf_s(" Количество свободных регионов выполняющегося процесса: % 8d\n", count);
-	printf_s(" Суммарный объем всех свободных регионов в адресном пространстве процесса: % 8d Кбайт\n", memory / 1024);
+	Print(0);
 }
 
 void TwoExemple() {
@@ -50,6 +38,8 @@ void TwoExemple() {
 	pb = 0;
 	LPVOID address = NULL;
 	LPVOID pMEM = NULL;
+	Print(1);
+	pb = 0;
 	while (VirtualQuery(LPCVOID(pb), &mbi, sizeof(MEMORY_BASIC_INFORMATION)) == sizeof(mbi))
 	{
 		if (mbi.RegionSize == size && mbi.State == MEM_FREE)
@@ -57,7 +47,7 @@ void TwoExemple() {
 			printf_s(" Адрес выделенного региона: %08x\n", mbi.BaseAddress);
 			address = mbi.BaseAddress;
 			VirtualAlloc(NULL, mbi.RegionSize, MEM_RESERVE, PAGE_READWRITE);
-			Sleep(10000);
+			Sleep(1);
 			pMEM = VirtualAlloc(NULL, mbi.RegionSize, MEM_COMMIT, PAGE_READWRITE);
 			break;
 		}
@@ -77,9 +67,37 @@ void TwoExemple() {
 		pb = pb + mbi.RegionSize;
 	}
 	printf_s(" Суммарный объем всех зарезервированных регионов в адресном пространстве процесса: % 8d Кбайт\n Количество зарезервированных регионов выполняющегося процесса: % 8d\n", memory / 1024, count);
-	
+
 	if (VirtualFree(pMEM, 0, MEM_RELEASE))
 	{
 		printf_s(" Возврат физической памяти произошел успешно!\n");
 	}
+}
+
+void Print(bool mem) {
+	count = 0;
+	memory = 0;
+	pb = 0;
+	printf_s(" %8s %20s(байты)\t%16s\n", "Адрес", "Размер региона", "Атрибут защиты");
+	while (VirtualQuery(LPCVOID(pb), &mbi, sizeof(MEMORY_BASIC_INFORMATION)) == sizeof(mbi))
+	{
+		if (mem && mbi.State == MEM_RESERVE)
+		{
+			printf_s(" %08x %16d %16d\n", mbi.BaseAddress, mbi.RegionSize, mbi.State);
+
+			count++;
+			memory += mbi.RegionSize;
+		}
+		else if(mbi.State == MEM_FREE)
+		{
+			if (pb > 0 && pb < pow(1024, 3))
+				printf_s(" %08x %16d %16d\n", mbi.BaseAddress, mbi.RegionSize, mbi.State);
+
+			count++;
+			memory += mbi.RegionSize;
+		}
+		pb = pb + mbi.RegionSize;
+	}
+	printf_s(" Количество зарезервированных регионов выполняющегося процесса: % 8d\n", count);
+	printf_s(" Суммарный объем всех зарезервированных регионов в адресном пространстве процесса: % 8d Кбайт\n", memory / 1024);
 }
